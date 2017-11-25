aa50831f5aff1460d450c646dfed3ef2eed7210a
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-25 10:08:24 -0800

    Using Class binding.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,13 +5,14 @@ import { Component } from '@angular/core';
     selector: 'courses',
     template: `
         <h2>{{title}}</h2>
-        <button class="btn btn-primary">Click Me!</button>
+        <button class="btn btn-primary" [class.active]="isActive">Click Me!</button>
     `
 })
 export class CoursesComponent {
     title = "List of courses";
     colspan = 2;
     courses;
+    isActive = true;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
