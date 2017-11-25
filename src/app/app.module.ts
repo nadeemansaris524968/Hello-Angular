34ad50878a8f67246173b47b32f18af5a62d80ca
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-25 13:50:18 -0800

    Add bootstrap form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
