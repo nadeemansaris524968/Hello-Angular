f626f8acb2df7eb19d3f297412e1821709a72110
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-26 07:57:31 -0800

    Extracting reusable Data Service - II.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,39 +5,41 @@ import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class DataService {
 
-  constructor(private http: Http, private url: string) { }
+    constructor(private http: Http, private url: string) { }
 
-  getAll() {
-    return this.http.get(this.url)
-      .catch(this.handleError);
-  }
+    getAll() {
+        return this.http.get(this.url)
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
 
-  create(resource) {
-    return this.http.post(this.url, JSON.stringify(resource))
-      .catch(this.handleError);
-  }
+    create(resource) {
+        return this.http.post(this.url, JSON.stringify(resource))
+            .catch(this.handleError);
+    }
 
-  update(resource) {
-    return this.http.put(this.url + '/' + resource.id, JSON.stringify(resource))
-      .catch(this.handleError);
-  }
+    update(resource) {
+        return this.http.put(this.url + '/' + resource.id, JSON.stringify(resource))
+            .catch(this.handleError);
+    }
 
-  delete(id) {
-    return this.http.delete(this.url + '/' + id).catch(this.handleError);
-  }
+    delete(id) {
+        return this.http.delete(this.url + '/' + id).catch(this.handleError);
+    }
 
-  private handleError(error: Response) {
-    if (error.status === 400)
-      return Observable.throw(new BadInputError(error.json()));
+    private handleError(error: Response) {
+        if (error.status === 400)
+            return Observable.throw(new BadInputError(error.json()));
 
-    if (error.status === 404)
-      return Observable.throw(new NotFoundError()); // Legitimate error, not saving
+        if (error.status === 404)
+            return Observable.throw(new NotFoundError()); // Legitimate error, not saving
 
-    return Observable.throw(new AppError(error));
-  }
+        return Observable.throw(new AppError(error));
+    }
 }
