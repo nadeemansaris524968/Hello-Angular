be78ad4db670a52e38de6bc12483473c2cd63594
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-25 18:09:11 -0800

    Implementing Nested FormGroup.
    Reactive version of ngModelGroup from Template driven approach.

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,16 +9,22 @@ import { Component } from '@angular/core';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
-    ],
-      UsernameValidators.shouldBeUnique),
-    password: new FormControl('', Validators.required)
+    account: new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace
+      ],
+        UsernameValidators.shouldBeUnique),
+      password: new FormControl('', Validators.required)
+    })
   });
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
+  }
+
+  login() {
+    
   }
 }
