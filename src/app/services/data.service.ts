37fa18b627a509e9e3268aae42061c1bb3d67101
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-26 07:26:23 -0800

    Extracting reusable Data Service.

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,43 @@
+import { BadInputError } from './../common/bad-input-error';
+import { NotFoundError } from '../common/not-found-error';
+import { AppError } from './../common/app.error';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class DataService {
+
+  constructor(private http: Http, private url: string) { }
+
+  getAll() {
+    return this.http.get(this.url)
+      .catch(this.handleError);
+  }
+
+  create(resource) {
+    return this.http.post(this.url, JSON.stringify(resource))
+      .catch(this.handleError);
+  }
+
+  update(resource) {
+    return this.http.put(this.url + '/' + resource.id, JSON.stringify(resource))
+      .catch(this.handleError);
+  }
+
+  delete(id) {
+    return this.http.delete(this.url + '/' + id).catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    if (error.status === 400)
+      return Observable.throw(new BadInputError(error.json()));
+
+    if (error.status === 404)
+      return Observable.throw(new NotFoundError()); // Legitimate error, not saving
+
+    return Observable.throw(new AppError(error));
+  }
+}
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,44 +1,11 @@
-import { BadInputError } from './../common/bad-input-error';
-import { NotFoundError } from '../common/not-found-error';
-import { AppError } from './../common/app.error';
+import { DataService } from './data.service';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
 
 @Injectable()
-export class PostService {
-  private url = 'http://jsonplaceholder.typicode.com/posts';
+export class PostService extends DataService {
 
-  constructor(private http: Http) { }
-
-  getPosts() {
-    return this.http.get(this.url)
-      .catch(this.handleError);
-  }
-
-  createPost(post) {
-    return this.http.post(this.url, JSON.stringify(post))
-      .catch(this.handleError);
-  }
-
-  updatePost(post) {
-    return this.http.put(this.url + '/' + post.id, JSON.stringify(post))
-      .catch(this.handleError);
-  }
-
-  deletePost(id) {
-    return this.http.delete(this.url + '/' + id).catch(this.handleError);
-  }
-
-  private handleError(error: Response) {
-    if (error.status === 400)
-      return Observable.throw(new BadInputError(error.json()));
-
-    if (error.status === 404)
-      return Observable.throw(new NotFoundError()); // Legitimate error, not saving
-
-    return Observable.throw(new AppError(error));
-  }
+  constructor(http: Http) {
+    super(http, 'http://jsonplaceholder.typicode.com/posts');
+   }
 }
