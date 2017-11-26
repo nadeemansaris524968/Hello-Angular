1258611c490aca537f60e0ee317c63fd69218ed5
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-11-26 07:18:17 -0800

    Common error handling method.

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -14,29 +14,31 @@ export class PostService {
   constructor(private http: Http) { }
 
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url)
+      .catch(this.handleError);
   }
 
   createPost(post) {
     return this.http.post(this.url, JSON.stringify(post))
-      .catch((error: Response) => {
-        if (error.status === 400)
-          return Observable.throw(new BadInputError(error.json()));
-
-        return Observable.throw(new AppError(error.json()));
-      });
+      .catch(this.handleError);
   }
 
   updatePost(post) {
-    return this.http.put(this.url + '/' + post.id, JSON.stringify(post));
+    return this.http.put(this.url + '/' + post.id, JSON.stringify(post))
+      .catch(this.handleError);
   }
 
   deletePost(id) {
-    return this.http.delete(this.url + '/' + id).catch((error: Response) => {
-      if (error.status === 404)
-        return Observable.throw(new NotFoundError()); // Legitimate error, not saving
+    return this.http.delete(this.url + '/' + id).catch(this.handleError);
+  }
 
-      return Observable.throw(new AppError(error));
-    });
+  private handleError(error: Response) {
+    if (error.status === 400)
+      return Observable.throw(new BadInputError(error.json()));
+
+    if (error.status === 404)
+      return Observable.throw(new NotFoundError()); // Legitimate error, not saving
+
+    return Observable.throw(new AppError(error));
   }
 }
